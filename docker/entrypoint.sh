831cf608544048cdd16a461212a3648722283d31
Felipe Vega <felipevegaesparza@gmail.com>
2026-03-25 22:53:53 -0300

    fix imgg 3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,22 @@ set -e
 
 cd /var/www/html
 
+# Ensure storage directories exist
+mkdir -p storage/app/public
+mkdir -p storage/framework/cache
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/logs
+mkdir -p bootstrap/cache
+
+# Fix storage permissions
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
+# Remove broken symlink if exists and recreate
+rm -f public/storage
+php artisan storage:link
+
 # Run migrations
 php artisan migrate --force
 
@@ -10,9 +26,6 @@ php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Fix storage permissions
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Start supervisor
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
